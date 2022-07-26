437eaa59186d8e0f7d252bd6e19d2349d985a9a5
Arjun P V <arjun.pv@ccs-technologies.com>
2022-07-27 04:44:22 +0530

    final changes

--- src/components/EnquiryComponent/index.tsx
+++ src/components/EnquiryComponent/index.tsx
@@ -4,7 +4,7 @@ export default function EnquiryComponent() {
   return (
     <div className="bg-[url('./images/bg-2.jpg')] bg-cover bg-no-repeat bg-center bg-no-repeat h-3/5 w-screen px-auto py-14 ">
       <h3 className="text-white font-semibold text-2xl">Got a question?</h3>
-      <div className="flex flex-col lg:max-w-md mx-4 md:mx-auto mt-4">
+      <div className="flex flex-col lg:max-w-md mx-4 lg:mx-auto mt-4">
         <div className="flex flex-row justify-center w-full">
           <input className="w-1/2 mb-4 p-2 rounded-sm text-sm" type="text" placeholder="Name" />
           <input className="w-1/2 ml-2 mb-4 p-2 rounded-sm text-sm" type="email" placeholder="Email" />
